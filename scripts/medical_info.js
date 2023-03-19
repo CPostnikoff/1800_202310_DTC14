db8d044f528e3f27c32c6dde9073cf21a71f557c
adambgoldsmith <adamgoldsmithmail@gmail.com>
2023-03-18 22:31:01 -0700

    Fix ailment steps

--- scripts/medical_info.js
+++ scripts/medical_info.js
@@ -8,10 +8,28 @@ function displayMedicalInfo() {
 
             document.getElementById("ailment-name").innerHTML = ailmentName
             document.getElementById("symptoms").innerHTML = ailmentSymptoms
-            ailmentTreatment.forEach((step, index) => {
-                document.getElementById(`step-${index}`).innerHTML = step
 
-            })
+            let parent = document.getElementById("steps-div")
+            let steps = []
+
+            ailmentTreatment.forEach((step, index) => {
+                let stepDiv = document.createElement('div')
+                    stepDiv.innerHTML =
+                        `<div class="step-box">
+                            <div class="row" style="margin-left: 5%; margin-right: 5%;">
+                                <div class="col-2 my-auto" style="text-align: end; padding-right: 35px;">
+                                    <b>${index + 1}.</b>
+                                </div>
+                                <div class="col-10" id="step-${index}">
+                                    ${step}
+                                </div>
+                            </div>
+                        </div>
+                        <hr>`;
+
+                steps.push(stepDiv)    
+            });
+            steps.forEach(step => parent.appendChild(step))
         })
     })
 }

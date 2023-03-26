2c3aba9d71b54c39c85335a5347b7f88cf3bb4f7
CPostnikoff <CameronJP@protonmail.com>
2023-03-25 17:37:15 -0700

    Add tag filtering to each_location.html

--- scripts/each_location.js
+++ scripts/each_location.js
@@ -1,3 +1,15 @@
+all_locations = []
+
+const button_selection = () => {
+    document.querySelectorAll("button").forEach(button => {
+        let id = button.getAttribute("id").toString()
+        button.addEventListener("click", function () {
+            console.log(id)
+            filterDisplayedLocations(id)
+        })
+    })
+}
+
 function correctedTitle(title) {
     to_cap = title.split("_");
     for (i = 0; i < to_cap.length; i++) {
@@ -14,10 +26,13 @@ function displayLocationsDynamically(collection) {
     db.collection(collection).get()
         .then(allLocations => {
             allLocations.forEach(doc => {
-                console.log(doc.data())
+                // console.log(doc.data())
                 var title = correctedTitle(doc.id);
                 var details = doc.data().description;
                 var image = doc.data().picture;
+                var tags = doc.data().tags;
+
+                all_locations.push([doc.id, tags])
 
                 let newcard = locationCardTemplate.content.cloneNode(true);
 
@@ -25,10 +40,24 @@ function displayLocationsDynamically(collection) {
                 newcard.querySelector('.location_description').innerHTML = details;
                 newcard.querySelector('.location_image').src = image;
                 newcard.querySelector('a').href = "./templates/location_page_template.html?docID=" + doc.id;
+                newcard.querySelector('.container').id = doc.id;
+
 
                 document.getElementById(collection + "-go-here").appendChild(newcard)
             })
         })
 }
 
+function filterDisplayedLocations(tag_to_filter) {
+    for (i = 0; i < all_locations.length; i++) {
+        if (all_locations[i][1].includes(tag_to_filter)) {
+            document.getElementById(all_locations[i][0]).style.display = "Block"
+        } else {
+            document.getElementById(all_locations[i][0]).style.display = "none"
+        }
+    }
+}
+
+
 displayLocationsDynamically("locations")
+button_selection()

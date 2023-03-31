4eb3d18a8db01e6bdb03def3cef787531eb4bfc8
mho879 <mho122@my.bcit.ca>
2023-03-30 20:44:28 -0700

    Update authentication.js to save date and time

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -22,6 +22,7 @@ var uiConfig = {
             email: user.email, //with authenticated user's ID (user.uid)
             country: "Canada", //optional default profile info
             favourites: [], //users favourites
+            date_created: new Date()
           }).then(function () {
             console.log("New user added to firestore");
             window.location.assign("main.html"); //re-direct to main.html after signup

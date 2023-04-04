a3bae30a9afb66efa9604f4cff72acd3279926e6
mho879 <mho122@my.bcit.ca>
2023-04-04 16:36:21 -0700

    Add weather icons to weather page
    Reads from OpenWeatherMap API and display relevant weather Icon

--- scripts/open_weather_map.js
+++ scripts/open_weather_map.js
@@ -29,11 +29,7 @@ function populate_fields(data) {
     document.getElementById('temp').innerHTML = Math.round(data.list[0].main.temp) + ' °C';
     document.getElementById('rain-chance').innerHTML = (data.list[0].pop * 100) + ' %';
     document.getElementById('wind-speed').innerHTML = data.list[0].wind.speed + ' m/s';
-    dateFormatToday = data.list[0].dt_txt.  split(' ')[0];
-
-
-    // Populate details for the next 5 days
-    
+  
     // Get the date for the next 5 days
     const today = new Date();
     var dayOne = today.getDate() +1;
@@ -43,25 +39,35 @@ function populate_fields(data) {
     var dayFive = today.getDate() + 5;
 
     // Find the document containing the date for the next 5 days
-    dayOneArray = findDocOfDate(data, replaceDay(dateFormatToday, dayOne) + ' 12:00:00');
-    dayTwoArray = findDocOfDate(data, replaceDay(dateFormatToday, dayTwo) + ' 12:00:00');
-    dayThreeArray = findDocOfDate(data, replaceDay(dateFormatToday, dayThree) + ' 12:00:00');
-    dayFourArray = findDocOfDate(data, replaceDay(dateFormatToday, dayFour) + ' 12:00:00');
-    dayFiveArray = findDocOfDate(data, replaceDay(dateFormatToday, dayFive) + ' 12:00:00');
+    dateFormatToday = data.list[0].dt_txt.split(' ')[0];
+    dayOneDocument = findDocOfDate(data, replaceDay(dateFormatToday, dayOne) + ' 12:00:00');
+    dayTwoDocument = findDocOfDate(data, replaceDay(dateFormatToday, dayTwo) + ' 12:00:00');
+    dayThreeDocument = findDocOfDate(data, replaceDay(dateFormatToday, dayThree) + ' 12:00:00');
+    dayFourDocument = findDocOfDate(data, replaceDay(dateFormatToday, dayFour) + ' 12:00:00');
+    dayFiveDocument = findDocOfDate(data, replaceDay(dateFormatToday, dayFive) + ' 12:00:00');
 
     // Populate the details for the next 5 days
-    $('#extraDayOne').html(dayOneArray.dt_txt.split(' ')[0].slice(5));
-    $('#extraDayTwo').html(dayTwoArray.dt_txt.split(' ')[0].slice(5));
-    $('#extraDayThree').html(dayThreeArray.dt_txt.split(' ')[0].slice(5));
-    $('#extraDayFour').html(dayFourArray.dt_txt.split(' ')[0].slice(5));
-    $('#extraDayFive').html(dayFiveArray.dt_txt.split(' ')[0].slice(5));
+    $('#extraDayOne').html(dayOneDocument.dt_txt.split(' ')[0].slice(5));
+    $('#extraDayTwo').html(dayTwoDocument.dt_txt.split(' ')[0].slice(5));
+    $('#extraDayThree').html(dayThreeDocument.dt_txt.split(' ')[0].slice(5));
+    $('#extraDayFour').html(dayFourDocument.dt_txt.split(' ')[0].slice(5));
+    $('#extraDayFive').html(dayFiveDocument.dt_txt.split(' ')[0].slice(5));
 
     // Populate the temperature for the next 5 days
-    $('#dayOneTemp').html(Math.round(dayOneArray.main.temp) + ' °C');
-    $('#dayTwoTemp').html(Math.round(dayTwoArray.main.temp) + ' °C');
-    $('#dayThreeTemp').html(Math.round(dayThreeArray.main.temp) + ' °C');
-    $('#dayFourTemp').html(Math.round(dayFourArray.main.temp) + ' °C');
-    $('#dayFiveTemp').html(Math.round(dayFiveArray.main.temp) + ' °C');
+    $('#dayOneTemp').html(Math.round(dayOneDocument.main.temp) + ' °C');
+    $('#dayTwoTemp').html(Math.round(dayTwoDocument.main.temp) + ' °C');
+    $('#dayThreeTemp').html(Math.round(dayThreeDocument.main.temp) + ' °C');
+    $('#dayFourTemp').html(Math.round(dayFourDocument.main.temp) + ' °C');
+    $('#dayFiveTemp').html(Math.round(dayFiveDocument.main.temp) + ' °C');
+
+    // Replace weather with icon
+    $('#currentWeather').html(replaceWeatherWithIcon(data.list[0].weather[0].main, '#currentWeather'));
+    $('#dayOneWeather').html(replaceWeatherWithIcon(dayOneDocument.weather[0].main, '#dayOneWeather'));
+    $('#dayTwoWeather').html(replaceWeatherWithIcon(dayTwoDocument.weather[0].main, '#dayTwoWeather'));
+    $('#dayThreeWeather').html(replaceWeatherWithIcon(dayThreeDocument.weather[0].main, '#dayThreeWeather'));
+    $('#dayFourWeather').html(replaceWeatherWithIcon(dayFourDocument.weather[0].main, '#dayFourWeather'));
+    $('#dayFiveWeather').html(replaceWeatherWithIcon(dayFiveDocument.weather[0].main, '#dayFiveWeather'));
+
 }
 
 // Find the document containing the listed date within a collection
@@ -83,6 +89,25 @@ function replaceDay(today, newDate) {
         return today.slice(0, -1) + newDate
     } else {
         return today.slice(0, -2) + newDate
+    }
+}
+
+// Replace retreived weather description with an icon
+function replaceWeatherWithIcon(weatherDescription, elementID) {
+    if (weatherDescription === 'Clouds') {
+        $(elementID).replaceWith('<span class="material-symbols-outlined">cloudy</span>');
+    } else if (weatherDescription === 'Rain') {
+        $(elementID).replaceWith('<span class="material-symbols-outlined">rainy</span>');
+    } else if (weatherDescription === 'Clear') {
+        $(elementID).replaceWith('<span class="material-symbols-outlined">sunny</span>');
+    } else if (weatherDescription === 'Snow') {
+        $(elementID).replaceWith('<span class="material-symbols-outlined">snowy</span>');
+    } else if (weatherDescription === 'Thunderstorm') {
+        $(elementID).replaceWith('<span class="material-symbols-outlined">thunderstorm</span>');
+    } else if (weatherDescription === 'Drizzle') {
+        $(elementID).replaceWith('<span class="material-symbols-outlined">drizzle</span>');
+    } else {
+        $(elementID).replaceWith("<p>'N/A'</p>");
     }
 }
 

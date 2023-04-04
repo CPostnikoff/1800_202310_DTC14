5da9feed978233f9661bed7940fb6eb19ea8a0c2
mho879 <mho122@my.bcit.ca>
2023-04-04 15:12:14 -0700

    Update weather page with temp of next 5 days

--- scripts/open_weather_map.js
+++ scripts/open_weather_map.js
@@ -25,10 +25,65 @@ function get_weather(lat, lon) {
 }
 
 function populate_fields(data) {
-    document.getElementById('city').innerHTML = data.city.name
-    document.getElementById('temp').innerHTML = Math.round(data.list[0].main.temp) + ' °C'
-    document.getElementById('rain-chance').innerHTML = (data.list[0].pop * 100) + ' %'
-    document.getElementById('wind-speed').innerHTML = data.list[0].wind.speed + ' m/s'
+    document.getElementById('city').innerHTML = data.city.name;
+    document.getElementById('temp').innerHTML = Math.round(data.list[0].main.temp) + ' °C';
+    document.getElementById('rain-chance').innerHTML = (data.list[0].pop * 100) + ' %';
+    document.getElementById('wind-speed').innerHTML = data.list[0].wind.speed + ' m/s';
+    dateFormatToday = data.list[0].dt_txt.  split(' ')[0];
+
+
+    // Populate details for the next 5 days
+    
+    // Get the date for the next 5 days
+    const today = new Date();
+    var dayOne = today.getDate() +1;
+    var dayTwo = today.getDate() +2;
+    var dayThree = today.getDate() +3;
+    var dayFour = today.getDate() +4;
+    var dayFive = today.getDate() + 5;
+
+    // Find the document containing the date for the next 5 days
+    dayOneArray = findDocOfDate(data, replaceDay(dateFormatToday, dayOne) + ' 12:00:00');
+    dayTwoArray = findDocOfDate(data, replaceDay(dateFormatToday, dayTwo) + ' 12:00:00');
+    dayThreeArray = findDocOfDate(data, replaceDay(dateFormatToday, dayThree) + ' 12:00:00');
+    dayFourArray = findDocOfDate(data, replaceDay(dateFormatToday, dayFour) + ' 12:00:00');
+    dayFiveArray = findDocOfDate(data, replaceDay(dateFormatToday, dayFive) + ' 12:00:00');
+
+    // Populate the details for the next 5 days
+    $('#extraDayOne').html(dayOneArray.dt_txt.split(' ')[0].slice(5));
+    $('#extraDayTwo').html(dayTwoArray.dt_txt.split(' ')[0].slice(5));
+    $('#extraDayThree').html(dayThreeArray.dt_txt.split(' ')[0].slice(5));
+    $('#extraDayFour').html(dayFourArray.dt_txt.split(' ')[0].slice(5));
+    $('#extraDayFive').html(dayFiveArray.dt_txt.split(' ')[0].slice(5));
+
+    // Populate the temperature for the next 5 days
+    $('#dayOneTemp').html(Math.round(dayOneArray.main.temp) + ' °C');
+    $('#dayTwoTemp').html(Math.round(dayTwoArray.main.temp) + ' °C');
+    $('#dayThreeTemp').html(Math.round(dayThreeArray.main.temp) + ' °C');
+    $('#dayFourTemp').html(Math.round(dayFourArray.main.temp) + ' °C');
+    $('#dayFiveTemp').html(Math.round(dayFiveArray.main.temp) + ' °C');
+}
+
+// Find the document containing the listed date within a collection
+// :param dateText: The date to search for, in the format YYYY-MM-DD HH:MM:SS
+function findDocOfDate(data, dateText) {
+    for (let i = 0; i < data.list.length; i++) {
+      if (data.list[i].dt_txt === dateText) {
+        // console.log(data.list[i])
+        return data.list[i];
+      }
+    }
+}
+
+// Update the date to a different day
+// :param today: The date to update, in the format YYYY-MM-DD
+// :param newDate: The new date, in the format DD
+function replaceDay(today, newDate) {
+    if (newDate <= 9) {
+        return today.slice(0, -1) + newDate
+    } else {
+        return today.slice(0, -2) + newDate
+    }
 }
 
 $(document).ready(function () {
